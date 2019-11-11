195f2cb5e95ab20c500c054102a8220da6cbdcb5
Daniel Siekiera <ds@optimondo.de>
2019-11-11 19:25:53 +0100

    Prüfung auf Time-Obj integriert. Menü wird immer sicher geladen

--- js/inject.js
+++ js/inject.js
@@ -5,7 +5,7 @@
  * Projekt:                   foe
  *
  * erstellt von:              Daniel Siekiera <daniel.siekiera@gmail.com>
- * zu letzt bearbeitet:       11.11.19, 16:36 Uhr
+ * zu letzt bearbeitet:       11.11.19, 19:17 Uhr
  *
  * Copyright © 2019
  *
@@ -71,66 +71,53 @@ i18nJS.onload = function(){
 (document.head || document.documentElement).appendChild(i18nJS);
 
 
-let jsid = setInterval(InjectCode, 5);
+let vendorScripts = [
+	'moment/moment-with-locales.min',
+	'CountUp/jquery.easy_number_animate.min',
+	'clipboard/clipboard.min',
+	'Tabslet/jquery.tabslet.min',
+	'ScrollTo/jquery.scrollTo.min',
+	'jQuery/jquery-resizable.min',
+	'tooltip/tooltip',
+	'tableSorter/table-sorter',
+];
 
-function InjectCode()
-{
-	// warten bis jQuery geladen wurde
-	if(typeof $ != 'undefined') {
-		let vendorScripts = [
-			'moment/moment-with-locales.min',
-			'CountUp/jquery.easy_number_animate.min',
-			'clipboard/clipboard.min',
-			'Tabslet/jquery.tabslet.min',
-			'ScrollTo/jquery.scrollTo.min',
-			'jQuery/jquery-resizable.min',
-			'tooltip/tooltip',
-			'tableSorter/table-sorter',
-		];
-
-		for (let vs in vendorScripts) {
-			if (vendorScripts.hasOwnProperty(vs)) {
-				let sc = document.createElement('script');
-				sc.src = chrome.extension.getURL('vendor/' + vendorScripts[vs] + '.js?v=' + v);
-				sc.onload = function () {
-					this.remove();
-				};
-				(document.head || document.documentElement).appendChild(sc);
-			}
-		}
-
-
-		let s = [
-			'helper',
-			'ant',
-			'menu',
-			'tavern',
-			'outposts',
-			'calculator',
-			'infoboard',
-			'productions',
-			'part-calc',
-			'read-buildings',
-			'settings',
-			'strategy-points',
-			'citymap'
-		];
-
-		// Scripte laden
-		for (let i in s) {
-			if (s.hasOwnProperty(i)) {
-				let sc = document.createElement('script');
-				sc.src = chrome.extension.getURL('js/web/' + s[i] + '.js?v=' + v);
-				sc.onload = function () {
-					this.remove();
-				};
-				(document.head || document.documentElement).appendChild(sc);
-			}
-		}
-
-		clearInterval(jsid);
+for (let vs in vendorScripts) {
+	if (vendorScripts.hasOwnProperty(vs)) {
+		let sc = document.createElement('script');
+		sc.src = chrome.extension.getURL('vendor/' + vendorScripts[vs] + '.js?v=' + v);
+		sc.onload = function () {
+			this.remove();
+		};
+		(document.head || document.documentElement).appendChild(sc);
 	}
 }
 
 
+let s = [
+	'helper',
+	'ant',
+	'menu',
+	'tavern',
+	'outposts',
+	'calculator',
+	'infoboard',
+	'productions',
+	'part-calc',
+	'read-buildings',
+	'settings',
+	'strategy-points',
+	'citymap'
+];
 
+// Scripte laden
+for (let i in s) {
+	if (s.hasOwnProperty(i)) {
+		let sc = document.createElement('script');
+		sc.src = chrome.extension.getURL('js/web/' + s[i] + '.js?v=' + v);
+		sc.onload = function () {
+			this.remove();
+		};
+		(document.head || document.documentElement).appendChild(sc);
+	}
+}
