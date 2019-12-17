cd792149e7e4ef0d681d49480f155c2070246478
Th3C0D3R <nico.fengler@gmx.de>
2019-12-17 14:09:27 +0100

    damit das nicht weg ist

--- js/web/settings.js
+++ js/web/settings.js
@@ -22,7 +22,7 @@ let Settings = {
 		Version: {
 			callback: 'VersionInfo',
 			title: i18n['Settings']['Version']['Title'],
-			desc: i18n['Settings']['Version']['Desc']
+			desc: (devMode === true ? i18n['Settings']['Version']['DescDebug'] : i18n['Settings']['Version']['Desc'])
 		},
 		GlobalSend : {
 			status: true,
