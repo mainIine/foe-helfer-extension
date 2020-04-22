e60f20e086f52940ff3651250357ebc304cc9655
Gindi4711 <55417498+andrgin@users.noreply.github.com>
2020-04-22 13:54:40 +0200

    tavernas => taverns, outpost Beute fix

--- js/web/outposts/js/outposts.js
+++ js/web/outposts/js/outposts.js
@@ -331,7 +331,7 @@ let Outposts = {
 					for (let CostResourceName in cost) {
 						if (CostResourceName === 'diplomacy') continue;
 
-						resourceCost += Math.ceil((cost[CostResourceName] - ResourceStock[CostResourceName]|0) / 5) * (goodProductionResourceId === 'egyptians_loot' ? 50 : 1000);
+						resourceCost += Math.max(Math.ceil((cost[CostResourceName] - ResourceStock[CostResourceName]|0) / 5) * (goodProductionResourceId === 'egyptians_loot' ? 50 : 1000), 0);
                     }
                 }
 				const resourceInStock = currStock[resourceID];
@@ -360,7 +360,7 @@ let Outposts = {
 						let CostResourceName = resourceIDs[CostResource];
 						if (CostResourceName === 'diplomacy' || CostResourceName === goodProductionResourceId) continue;
 
-						resourceSumAfter += Math.ceil((sums[CostResourceName] - ResourceStock[CostResourceName]|0) / 5) * (goodProductionResourceId === 'egyptians_loot' ? 50 : 1000);
+						resourceSumAfter += Math.max(Math.ceil((sums[CostResourceName] - ResourceStock[CostResourceName]|0) / 5) * (goodProductionResourceId === 'egyptians_loot' ? 50 : 1000), 0);
 					}
 				}
 				else {
