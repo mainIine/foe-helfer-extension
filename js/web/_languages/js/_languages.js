47e70b423e28f4e69fc41b2e5497f7ebaa3fd0b8
Tiago Pereira <daragao689@gmail.com>
2020-05-17 14:59:02 -0300

    Updates

--- js/web/_languages/js/_languages.js
+++ js/web/_languages/js/_languages.js
@@ -1,18 +1,19 @@
 ﻿let Languages = {
-	PossibleLanguages: {
-		cs: 'Český',
-		de: 'Deutsch',
-		en: 'English',
-		es: 'Español',
-		fr: 'Français',
-		hu: 'Magyar',
-		it: 'Italiano',
-		nl: 'Nederlands',
-		pl: 'Polski',
-		pt: 'Português',
-		ro: 'Română',
-		ru: 'Русский',
-		sk: 'Slovenčina',
-		sv: 'Svenska'
-	},
+    PossibleLanguages: {
+        cs: 'Český',
+        de: 'Deutsch',
+        en: 'English',
+        es: 'Español',
+        fr: 'Français',
+        hu: 'Magyar',
+        it: 'Italiano',
+        nl: 'Nederlands',
+        pl: 'Polski',
+        pt: 'Português',
+        pt_BR: 'Português do Brasil'
+        ro: 'Română',
+        ru: 'Русский',
+        sk: 'Slovenčina',
+        sv: 'Svenska'
+    },
 };
