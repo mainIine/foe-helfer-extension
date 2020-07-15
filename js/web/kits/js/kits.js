32233ff3b5055777780a3554f47a132633bb27cd
Daniel Siekiera <ds@optimondo.de>
2020-07-15 12:18:45 +0200

    Kits - Modul v2 startet

--- js/web/kits/js/kits.js
+++ js/web/kits/js/kits.js
@@ -6,7 +6,6 @@
  *
  * erstellt von:              Daniel Siekiera <daniel.siekiera@gmail.com>
  * erstellt am:	              27.03.20, 15:37 Uhr
- * zuletzt bearbeitet:       27.03.20, 15:32 Uhr
  *
  * Copyright © 2020
  *
@@ -15,12 +14,12 @@
 
 /**
  *
- * @type {{BuildingSelectionKits: null, init: Kits.init, GetTabContent: (function(): string), KnownKits: null, ReadSets: Kits.ReadSets, GetInvententoryArray: (function(): []), globCnt: number, SetTabs: Kits.SetTabs, GetTabs: (function(): string), isChecked: [], BuildBox: Kits.BuildBox, setBuildings: [], ReadSelectionKits: Kits.ReadSelectionKits, CreateBody: Kits.CreateBody, BuildingSets: null, Tabs: [], ScanInvetory: Kits.ScanInvetory, SetTabContent: Kits.SetTabContent, TabsContent: [], Inventory: null, setSingles: []}}
+ * @type {{BuildingSelectionKits: null, init: Kits.init, GetTabContent: (function(): string), ReadSets: Kits.ReadSets, GetInvententoryArray: (function(): []), globCnt: number, SetTabs: Kits.SetTabs, GetTabs: (function(): string), isChecked: [], BuildBox: Kits.BuildBox, setBuildings: [], ReadSelectionKits: Kits.ReadSelectionKits, CreateBody: Kits.CreateBody, BuildingSets: null, Tabs: [], ScanInvetory: Kits.ScanInvetory, SetTabContent: Kits.SetTabContent, KitsjSON: null, TabsContent: [], Inventory: null, setSingles: []}}
  */
 let Kits = {
 
 	//
-	KnownKits: null,
+	KitsjSON: null,
 
 	// Lager
 	Inventory: null,
@@ -49,6 +48,36 @@ let Kits = {
 	TabsContent: [],
 
 
+	/**
+	 * Get all sets from the server
+	 */
+	init: ()=> {
+
+		let data = localStorage.getItem('KnownKitsData');
+
+		if(data === null || MainParser.checkNextUpdate('KnownKitsDate') === true){
+			MainParser.loadJSON('https://cache.foe-rechner.de/kits/sets.json', (data)=>{
+
+				localStorage.setItem('KnownKitsData', data);
+				localStorage.setItem('KnownKitsDate', MainParser.getAddedDateTime(48));
+
+				Kits.KitsjSON = JSON.parse(data);
+				Kits.BuildBox();
+			});
+
+		} else {
+			Kits.KitsjSON = JSON.parse(data);
+			Kits.BuildBox();
+			//Kits.ReadSets();
+		}
+	},
+
+
+	/**
+	 * Create the box
+	 *
+	 * @constructor
+	 */
 	BuildBox: ()=> {
 
 		// zurück setzen
@@ -56,11 +85,6 @@ let Kits = {
 		Kits.setBuildings = [];
 		Kits.setSingles = [];
 		Kits.globCnt = 0;
-
-		Kits.ReadSets();
-		Kits.ReadSelectionKits();
-		Kits.ScanInvetory();
-
 
 		if( $('#kits').length === 0 )
 		{
@@ -78,417 +102,233 @@ let Kits = {
 			HTML.CloseOpenBox('kits');
 		}
 
-		Kits.CreateBody();
+		Kits.ReadSets();
+
+		// Kits.CreateBody();
 	},
 
 
 	ReadSets: ()=> {
 		let inv = Kits.GetInvententoryArray(),
-			bs = Kits.BuildingSets,
-			bsk = Kits.BuildingSelectionKits;
-
-
-		// Sets durchsteppen
-		for(let b in bs)
-		{
-			if(!bs.hasOwnProperty(b)){
-				break;
-			}
-
-			let bCnt = Kits.globCnt;
-
-			Kits.setBuildings[bCnt] = {
-				setname:  bs[b]['name'],
-				orgSize: bs[b]['cityEntityIds'].length,
-				isSize: 0,
-				items: []
-			};
-
-			// Array mit cityEntityIds
-			let cEIds = bs[b]['cityEntityIds'],
-				cnt = 0;
-
-			// durch die Gebäude eines Sets springen
-			for(let i = 0; i < cEIds.length; i++)
-			{
-				let eID = cEIds[i];
-
-				// als geprüft "merken"
-				Kits.isChecked.push(eID);
-
-				// checken ob es auch im Inventar des Spielers ist
-				let item = inv.find(b => b['item']['cityEntityId'] === eID);
-
-				// ... ja, ist enthalten
-				if(item !== undefined)
-				{
-					if(Kits.setBuildings[bCnt]['items'][cnt] === undefined)
-					{
-						Kits.setBuildings[bCnt]['items'][cnt] = [];
-						Kits.setBuildings[bCnt]['items'][cnt]['stage'] = [];
-					}
-
-					Kits.setBuildings[bCnt]['items'][cnt]['stage'] = item;
-
-					Kits.setBuildings[bCnt]['isSize']++;
-				}
-
-				// prüfen ob es evt. schon ein Kit gibt
-				for(let e in bsk)
-				{
-					if(!bsk.hasOwnProperty(e)){
-						break;
-					}
-
-					let era = bsk[e]['eraOptions'][CurrentEra]['options'];
-
-					// ist das erste Gebäude das aktuelle Stufe 1?
-					if(era[0]['itemAssetName'] === eID && era[1]['itemAssetName'].includes('upgrade_kit') === true){
-
-						let ekID = era[1]['itemAssetName'],
-							itemKit = inv.find(b => b['itemAssetName'] === ekID);
-
-						Kits.isChecked.push(ekID);
-
-						if(itemKit !== undefined){
-							if(Kits.setBuildings[bCnt]['items'][cnt] === undefined)
-							{
-								Kits.setBuildings[bCnt]['items'][cnt] = [];
-							}
-
-							if(Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] === undefined)
-							{
-								Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = null;
-							}
-
-							Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = itemKit;
-						}
-					}
-				}
-
-				if(Kits.setBuildings[bCnt]['items'][cnt] !== undefined)
-				{
-					cnt++;
-				}
-
-				Kits.globCnt++;
-			}
-		}
-	},
-
-
-	ReadSelectionKits: ()=> {
-		let inv = Kits.GetInvententoryArray(),
-			bsk = Kits.BuildingSelectionKits;
-
-		// Sets durchsteppen
-		for(let b in bsk)
-		{
-			if(!bsk.hasOwnProperty(b)){
-				break;
-			}
-
-			let bCnt = Kits.globCnt,
-				eraItems = bsk[b]['eraOptions'][CurrentEra]['options'],
-				cnt = 0;
-
-			Kits.setBuildings[bCnt] = {
-				setname:  bsk[b]['name'],
-				orgSize: false,
-				isSize: 0,
-				items: []
-			};
-
-
-			for(let i = 0; i < eraItems.length; i++)
-			{
-				let aName = eraItems[i]['itemAssetName'],
-					name = eraItems[i]['name'];
-
-				if(aName !== '' && Kits.isChecked.includes(aName) === false){
-
-					// Es ist kein Upgrade Kit, also Stufe 1
-					if(aName.includes('upgrade_kit') === false)
-					{
-						let iItem = inv.find(obj => (obj['item']['cityEntityId'] === aName));
-
-						if(iItem !== undefined){
-							if(Kits.setBuildings[bCnt]['items'][cnt] === undefined)
-							{
-								Kits.setBuildings[bCnt]['items'][cnt] = [];
-								Kits.setBuildings[bCnt]['items'][cnt]['stage'] = [];
-							}
-
-							Kits.setBuildings[bCnt]['items'][cnt]['stage'] = iItem;
-
-							Kits.isChecked.push(aName);
-						}
-
-						// ist das nächste Object ein Upgrade-Kit? Dann gleich mit dazu
-						if(eraItems[1]['itemAssetName'].includes('upgrade_kit') === true){
-							let aName = eraItems[1]['itemAssetName'],
-								name = eraItems[1]['name'],
-								ukItem = inv.find(obj => (obj['name'] === name));
-
-							if(ukItem !== undefined){
-								if(Kits.setBuildings[bCnt]['items'][cnt] === undefined)
-								{
-									Kits.setBuildings[bCnt]['items'][cnt] = [];
-									Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = [];
-								}
-
-								Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = ukItem;
-
-								Kits.isChecked.push(aName);
-							}
-						}
-					}
-
-					// es gibt nur ein Kit ohne Stufe 1 oder es ist ein Selection-Kit
-					else if(aName.includes('upgrade_kit') === true) {
-						let sName = aName.replace('upgrade', 'selection'),
-							uName = aName,
-							fItem = undefined,
-							fName = null,
-							itemUk = inv.find(obj => (obj['itemAssetName'] === uName)),
-							itemSk = inv.find(obj => (obj['itemAssetName'] === sName));
-
-						if(itemUk !== undefined){
-							fItem = itemUk;
-							fName = uName;
-
-						} else if(itemSk !== undefined){
-							fItem = itemSk;
-							fName = sName;
-						}
-
-						if(fItem !== undefined){
-							if(Kits.setBuildings[bCnt]['items'][cnt] === undefined)
-							{
-								Kits.setBuildings[bCnt]['items'][cnt] = [];
-								Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = [];
-							}
-
-							Kits.setBuildings[bCnt]['items'][cnt]['upgrade'] = fItem;
-
-							Kits.isChecked.push(fName);
-						}
-					}
-				}
-
-				if(Kits.setBuildings[bCnt]['items'][cnt] !== undefined)
-				{
-					cnt++;
-				}
-			}
-
-			Kits.globCnt++;
-			// console.log('---------------------------------');
-		}
-	},
-
-
-	/**
-	 * Im Inventar nach einzelenen Selection-Kits suchen
-	 *
-	 * @constructor
-	 */
-	ScanInvetory: ()=> {
-		let inv = Kits.GetInvententoryArray();
-
-		for(let i in inv)
-		{
-			if(!inv.hasOwnProperty(i)){
-				break;
-			}
-
-			let aName = inv[i]['itemAssetName'];
-
-			if(aName.includes('selection_kit') && aName !== '' && Kits.isChecked.includes(aName) === false)
-			{
-				Kits.setSingles.push(inv[i]);
-				Kits.isChecked.push(aName);
-			}
-		}
-	},
-
-
-	// Box-Content zusammensetzen und einfügen
-	CreateBody: ()=> {
-
-		Kits.Tabs = [];
-		Kits.TabsContent = [];
-
-		Kits.SetTabs('building-kits', i18n('Boxes.Kits.TabBuildingKits'));
-
+			kits = Kits.KitsjSON;
 
 		let t = '<table class="foe-table">';
 
+		t += 	`<tr>
+					<th></th>
+					<th>${i18n('Boxes.Kits.Name')}</th>
+					<th></th>
+					<th>${i18n('Boxes.Kits.KitName')}</th>
+				</tr>`;
 
-		t += '<tr>' +
-			'<th></th>' +
-			'<th>' + i18n('Boxes.Kits.Name') + '</th>' +
-			'<th></th>' +
-			'<th>' + i18n('Boxes.Kits.KitName') + '</th>' +
-			'</tr>';
-
-
-		for(let i in Kits.setBuildings)
+		// Sets durchsteppen
+		for(let set in kits)
 		{
-			if(!Kits.setBuildings.hasOwnProperty(i)){
+			if(!kits.hasOwnProperty(set)){
 				break;
 			}
 
-			// beide leer, überspringen
-			if(Kits.setBuildings[i]['items'].length === 0){
-				continue;
-			}
+			let buildings = [],
+				assetRow = [],
+				show = false;
 
-			t += '<tr><th colspan="4">' + Kits.setBuildings[i]['setname'] + '</th></tr>';
-
-			for(let gb in Kits.setBuildings[i]['items'])
+			for(let i in kits[set]['buildings'])
 			{
-				if(!Kits.setBuildings[i]['items'].hasOwnProperty(gb)){
+				if(!kits[set]['buildings'].hasOwnProperty(i)){
 					break;
 				}
 
-				let gbs = Kits.setBuildings[i]['items'][gb],
-					rowTd = '';
+				const building = kits[set]['buildings'][i];
+				let itemRow = [];
 
-				// Stufe 1
-				if(gbs['stage'] !== undefined){
-					let item = gbs['stage'],
-						aName = item['itemAssetName'],
-						url = MainParser.InnoCDN + 'assets/city/buildings/' + [aName.slice(0, 1), '_SS', aName.slice(1)].join('') + '.png';
+				// Level 1
+				let itemL1 = inv.find(el => el['item']['cityEntityId'] === building['first']);
 
-					rowTd += '<td class="text-center"><img class="kits-image" src="' + url + '" alt="' + item['name'] + '" /></td>';
-					rowTd += '<td>' + item['name'] + '<br>' + i18n('Boxes.Kits.InStock') + ': <strong class="text-warning">' + item['inStock'] + '</strong></td>';
-
-				} else {
-					rowTd += '<td colspan="2"></td>';
+				if(itemL1)
+				{
+					itemRow.push({
+						type: 'first',
+						item: itemL1
+					})
 				}
 
-				// UpgradeKit
-				if(gbs['upgrade'] !== undefined){
-					let item = gbs['upgrade'],
-						aName = item['itemAssetName'],
-						url = MainParser.InnoCDN + 'assets/shared/icons/reward_icons/reward_icon_' + aName + '.png';
+				// Upgrade Kit
+				if(building['update'])
+				{
+					let itemUgr = inv.find(el => el['itemAssetName'] === building['update']);
 
-					rowTd += '<td><img class="kits-image" src="' + url + '" alt="' + item['name'] + '" /></td>';
-					rowTd += '<td class="text-center">' + item['name'] + '<br>' + i18n('Boxes.Kits.InStock') + ': <strong class="text-warning">' + item['inStock'] + '</strong></td>';
-
-				} else {
-					rowTd += '<td colspan="2"></td>';
+					if(itemUgr)
+					{
+						itemRow.push({
+							type: 'update',
+							item: itemUgr
+						})
+					}
 				}
 
-				t += '<tr>' + rowTd + '</tr>';
+				if(itemRow.length){
+					buildings.push(itemRow);
+					show = true;
+				}
+			}
+
+
+			// Building has asset buildings?
+			if(kits[set]['assets'])
+			{
+				for(let a in kits[set]['assets'])
+				{
+					if(!kits[set]['assets'].hasOwnProperty(a)){
+						break;
+					}
+
+					let asset = inv.find(el => el['item']['cityEntityId'] === kits[set]['assets'][a]);
+
+					if(asset) {
+						assetRow.push({
+							element: 'asset',
+							item: asset
+						})
+					}
+				}
+			}
+
+			if(assetRow.length){
+				show = true;
+			}
+
+			let kitRow = [];
+
+			if(kits[set]['kit']){
+				let k = inv.find(el => el['itemAssetName'] === kits[set]['kit']);
+
+				if(k){
+					kitRow.push({
+						type: 'update',
+						item: k
+					})
+				}
+			}
+
+			if(kitRow.length){
+				show = true;
+			}
+
+			if(show)
+			{
+				t += `<tr><th colspan="4" class="head">${kits[set]['name']}</th></tr>`;
+
+				if(buildings)
+				{
+					buildings.forEach((e) => {
+						let rowTd = '';
+
+						if(e[0]['type'] === 'first'){
+
+							rowTd += Kits.ItemTd(e[0]);
+
+							if(e[1] === undefined){
+								rowTd += '<td colspan="2"></td>';
+
+							} else {
+								rowTd += Kits.ItemTd(e[1]);
+							}
+
+						} else if(e[0]['type'] === 'update') {
+							rowTd += '<td colspan="2"></td>';
+							rowTd += Kits.ItemTd(e[0]);
+						}
+
+						t += '<tr>' + rowTd + '</tr>';
+					});
+				}
+
+				// Asset listing
+				if(assetRow.length)
+				{
+					t += `<tr><td colspan="4" class="assets-header">${i18n('Boxes.Kits.Extensions')}</td></tr>`;
+					let rowTd = '<td colspan="4"><div class="assets-row">';
+
+					assetRow.forEach((e) => {
+						rowTd += Kits.ItemDiv(e);
+					});
+
+					rowTd += '</div></td>';
+
+					t += '<tr>' + rowTd + '</tr>';
+				}
+
+				// Kit listing
+				if(kitRow.length)
+				{
+					t += `<tr><td colspan="4" class="assets-header">${i18n('Boxes.Kits.SelectionKit')}</td></tr>`;
+
+					let rowTd = Kits.ItemTd(kitRow[0]);
+					rowTd += '<td colspan="2"></td>';
+
+					t += '<tr>' + rowTd + '</tr>';
+				}
 			}
 		}
 
 		t += '</table>';
 
-		Kits.SetTabContent('building-kits', t);
-
-		// -------------------------------------------------------------------------------------------------------------
-		// -------------------------------------------------------------------------------------------------------------
+		$('#kitsBody').html(t);
+	},
 
 
-		Kits.SetTabs('selection-kits', i18n('Boxes.Kits.TabSelectionKits'));
+	/**
+	 * Create two td's for a level1 oder update building
+	 *
+	 * @param el
+	 * @returns {string}
+	 * @constructor
+	 */
+	ItemTd: (el)=> {
+		let item = el['item'],
+			aName = item['itemAssetName'],
+			td = '',
+			url;
 
-		let ts = '<div class="selection-wrapper">';
+		if(el['type'] === 'first'){
+			url = MainParser.InnoCDN + 'assets/city/buildings/' + [aName.slice(0, 1), '_SS', aName.slice(1)].join('') + '.png';
 
-		for(let i in Kits.setSingles)
-		{
-			if(!Kits.setSingles.hasOwnProperty(i)){
-				break;
-			}
-
-			let item = Kits.setSingles[i],
-				aName = item['itemAssetName'],
-				url = MainParser.InnoCDN + 'assets/shared/icons/reward_icons/reward_icon_' + aName + '.png';
-
-			ts += '<div class="item-wrap">' +
-				'<div class="item">' +
-				'<div class="item-image">' +
-				'<img src="' + url + '" alt="' + item['name'] + '">' +
-				'</div>' +
-				'<div class="item-name">' +
-				item['name'] + '<br>' +
-				i18n('Boxes.Kits.InStock') + ': <strong>' + item['inStock'] + '</strong>' +
-				'</div>' +
-				'</div>' +
-				'</div>';
+		} else if (el['type'] === 'update') {
+			url = MainParser.InnoCDN + 'assets/shared/icons/reward_icons/reward_icon_' + aName + '.png';
 		}
 
-		ts += '</div>';
+		td += '<td class="text-center"><img class="kits-image" src="' + url + '" alt="' + item['name'] + '" /></td>';
+		td += '<td>' + item['name'] + '<br>' + i18n('Boxes.Kits.InStock') + ': <strong class="text-warning">' + item['inStock'] + '</strong></td>';
 
-		Kits.SetTabContent('selection-kits', ts);
-
-		// -------------------------------------------------------------------------------------------------------------
-		// -------------------------------------------------------------------------------------------------------------
-
-		// fertige Tabelle zusammen setzten
-		let h = [];
-
-		h.push('<div class="kit-tabs tabs">');
-		h.push( Kits.GetTabs() );
-		h.push( Kits.GetTabContent() );
-		h.push('</div>');
-
-
-		$('#kitsBody').html( h.join('') ).promise().done(function(){
-			$('.kit-tabs').tabslet({active: 1});
-		});
+		return td;
 	},
 
 
 	/**
-	 * Note a  tab
+	 * Create a div-row for assets of a set
 	 *
-	 * @param id
-	 * @param label
-	 */
-	SetTabs: (id, label)=>{
-		Kits.Tabs.push(`<li class="${id} long-tab game-cursor"><a href="#${id}" class="game-cursor"><span>${label}</span></a></li>`);
-	},
-
-
-	/**
-	 * Gibt alle gemerkten Tabs aus
-	 *
+	 * @param el
 	 * @returns {string}
+	 * @constructor
 	 */
-	GetTabs: ()=> {
-		return '<ul class="horizontal">' + Kits.Tabs.join('') + '</ul>';
+	ItemDiv: (el)=> {
+		let item = el['item'],
+			aName = item['itemAssetName'],
+			url = MainParser.InnoCDN + 'assets/city/buildings/' + [aName.slice(0, 1), '_SS', aName.slice(1)].join('') + '.png';
+
+		return 	`<div class="item-asset">
+					<img class="asset-image" src="${url}" alt="${item['name']}" /><br>
+					${item['name']}<br>${i18n('Boxes.Kits.InStock')}: <strong class="text-warning">${item['inStock']}</strong>
+				</div>`;
 	},
 
 
 	/**
-	 * Speichert BoxContent zwischen
+	 * Return MainParser.Inventory as array
 	 *
-	 * @param id
-	 * @param content
+	 * @returns {[]}
+	 * @constructor
 	 */
-	SetTabContent: (id, content)=>{
-		Kits.TabsContent.push('<div id="' + id + '">' + content + '</div>');
-	},
-
-
-	/**
-	 * Setzt alle gespeicherten Tabs zusammen
-	 *
-	 * @returns {string}
-	 */
-	GetTabContent: ()=> {
-		return Kits.TabsContent.join('');
-	},
-
-
-	/**
-	 * Liefert MainParser.Inventory als Array zurück
-	 * 
-	 * @returns{[]}
-	 * */
 	GetInvententoryArray: () => {
 		let Ret = [];
 		for (let i in MainParser.Inventory) {
@@ -498,7 +338,7 @@ let Kits = {
 		}
 
 		return Ret;
-    },
+    }
 };
 
 // Updatestufen der Eventgebäude
