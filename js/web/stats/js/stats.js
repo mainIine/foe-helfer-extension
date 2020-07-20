88b602c58fb970b70771ca7c1084f0e7679425c4
Gindi4711 <55417498+andrgin@users.noreply.github.com>
2020-07-20 22:51:21 +0200

    Stats: Übersicht nach dem GG Ende

--- js/web/stats/js/stats.js
+++ js/web/stats/js/stats.js
@@ -1,34 +1,12 @@
 // Guild Battlegrounds leader board log
+// Gildengefechte
 FoEproxy.addHandler('GuildBattlegroundService', 'getPlayerLeaderboard', async (data, postData) => {
-	const r = data.responseData;
-	if (!Array.isArray(r)) {
-		return;
-	}
-	const players = r.reduce((acc, it) => {
-		acc[it.player.player_id] = {
-			id: it.player.player_id,
-			n: it.negotiationsWon || 0,
-			b: it.battlesWon || 0,
-			r: it.rank || 1
-		};
-		return acc;
-	}, {});
-	const timeNow = MainParser.getCurrentDate();
+	Stats.HandlePlayerLeaderboard(data.responseData);
+});
 
-    await IndexDB.getDB();
-
-    await IndexDB.db.statsGBGPlayers.add({
-		date: timeNow,
-		players
-	});
-
-	const playersForCache = r.map(({player}) => ({
-		id: player.player_id,
-		name: player.name,
-		avatar: player.avatar,
-		date: timeNow
-	}));
-	await IndexDB.db.statsGBGPlayerCache.bulkPut(playersForCache);
+// Gildengefechte
+FoEproxy.addHandler('GuildBattlegroundStateService', 'getState', async (data, postData) => {
+	Stats.HandlePlayerLeaderboard(data.responseData['playerLeaderboardEntries']);
 });
 
 // Reward log
@@ -1342,4 +1320,36 @@ let Stats = {
 			});
 		}
 	},
+
+	/* Handlers */
+	HandlePlayerLeaderboard: async (r) => {
+		if (!Array.isArray(r)) {
+			return;
+		}
+		const players = r.reduce((acc, it) => {
+			acc[it.player.player_id] = {
+				id: it.player.player_id,
+				n: it.negotiationsWon || 0,
+				b: it.battlesWon || 0,
+				r: it.rank || 1
+			};
+			return acc;
+		}, {});
+		const timeNow = MainParser.getCurrentDate();
+
+		await IndexDB.getDB();
+
+		await IndexDB.db.statsGBGPlayers.add({
+			date: timeNow,
+			players
+		});
+
+		const playersForCache = r.map(({ player }) => ({
+			id: player.player_id,
+			name: player.name,
+			avatar: player.avatar,
+			date: timeNow
+		}));
+		await IndexDB.db.statsGBGPlayerCache.bulkPut(playersForCache);		
+    },
 };
