484aee43f87de6adbc741cf1a9ecc250a1ae3da6
Daniel Siekiera <ds@optimondo.de>
2020-07-21 21:42:56 +0200

    Dropdown arrow animation

--- js/web/_helper/js/_helper.js
+++ js/web/_helper/js/_helper.js
@@ -490,14 +490,14 @@ let HTML = {
 
 		for (const dropdown of document.querySelectorAll(".custom-select-wrapper")) {
 			dropdown.addEventListener('click', function() {
-				this.querySelector('.custom-select').classList.toggle('open');
+				this.querySelector('.custom-select').classList.toggle('dd-open');
 			})
 		}
 
 		window.addEventListener('click', function(e) {
 			for (const select of document.querySelectorAll('.custom-select')) {
 				if (!select.contains(e.target)) {
-					select.classList.remove('open');
+					select.classList.remove('dd-open');
 				}
 			}
 		});
