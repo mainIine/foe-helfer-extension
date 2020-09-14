9f2224698c93151ed96599bc8ab236ec6da7fe1b
GeniusTimo <mail@geniustimo.de>
2020-09-14 23:03:38 +0200

    BattleAssist überarbeitet Die Warnung gerne überarbeiten, was Sprache & CSS angeht 😅

--- js/web/battle-assist/js/battle-assist.js
+++ js/web/battle-assist/js/battle-assist.js
@@ -1,34 +1,46 @@
 FoEproxy.addHandler('BattlefieldService', 'all', (data, postData) => {
-    HTML.CloseOpenBox('battleAssistStopDialog');
-
+    HTML.CloseOpenBox('battleAssistNextEraDialog'); HTML.CloseOpenBox('battleAssistRogueDialog');
     const state = data.responseData.__class__ === 'BattleRealm' ? data.responseData.state : data.responseData;
-    if (state.__class__ !== 'BattleRealmState') {
-        return;
-    }
-
+    if (state.__class__ !== 'BattleRealmState') return;
     const { winnerBit, unitsOrder, ranking_data } = state;
-    if (winnerBit !== 1) { return; } // 1 - win
-
-    if (!ranking_data.nextArmy) { return }
-
-    const atLeastOneNotRogue = unitsOrder.filter(it => it.teamFlag === 1 && it.currentHitpoints).some(it => it.unitTypeId != 'rogue');
-    const haveAliveUnits = unitsOrder.filter(it => it.teamFlag === 1 && it.currentHitpoints).length;
-    if (haveAliveUnits && !atLeastOneNotRogue) {
-        BattleAssist.ShowStopNextAttackDialog();
+    let alive = [], nextEraUnitDead = false;
+    for (const unit of unitsOrder.filter(e => e.teamFlag === 1)) {
+        if (unit.currentHitpoints) {
+            alive.push(unit.unitTypeId);
+        } else {
+            const unitEra = Unit.Types.find(e => e.unitTypeId === unit.unitTypeId)?.minEra;
+            if (Technologies.Eras[unitEra] > CurrentEraID) nextEraUnitDead = true;
+        }
     }
+    if (nextEraUnitDead) return BattleAssist.ShowNextEraDialog();
+    if (winnerBit !== 1 || !ranking_data.nextArmy) return;
+    if (alive.filter(e => e !== 'rogue')) return BattleAssist.ShowRogueDialog();
 });
 
 let BattleAssist = {
-    ShowStopNextAttackDialog: () => {
+    ShowNextEraDialog: () => {
+        HTML.AddCssFile('battle-assist');
+        
+        HTML.Box({
+            'id': 'battleAssistNextEraDialog',
+            'title': i18n('Boxes.BattleAssist.Title'),
+            'auto_close': true,
+            'dragdrop': false,
+            'minimize': false
+        });
+        $('#battleAssistNextEraDialogBody').html(`${i18n('Boxes.BattleAssist.Text.NextEra')}`);
+    },
+
+    ShowRogueDialog: () => {
         HTML.AddCssFile('battle-assist');
 
         HTML.Box({
-			'id': 'battleAssistStopDialog',
-			'title': i18n('Boxes.BattleAssist.Title'),
-			'auto_close': true,
-			'dragdrop': false,
-			'minimize': false
-		});
-        $('#battleAssistStopDialogBody').html(`<div class="blink">${i18n('Boxes.BattleAssist.Text')}</div>`);
+            'id': 'battleAssistRogueDialog',
+            'title': i18n('Boxes.BattleAssist.Title'),
+            'auto_close': true,
+            'dragdrop': false,
+            'minimize': false
+        });
+        $('#battleAssistRogueDialogBody').html(`<div class="blink">${i18n('Boxes.BattleAssist.Text.Rogue')}</div>`);
     },
 };
