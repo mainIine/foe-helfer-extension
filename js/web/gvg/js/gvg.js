5cc5edd7ac3219595a80f192852a20b6cbcb733f
outoftheline <yvi@outoftheline.net>
2021-03-28 17:01:21 +0200

    GvG Sieges

--- js/web/gvg/js/gvg.js
+++ js/web/gvg/js/gvg.js
@@ -2,8 +2,12 @@ FoEproxy.addHandler('ClanBattleService', 'grantIndependence', (data, postData) =
     GvG.CountIndepences(data.responseData.__class__);
 });
 
+FoEproxy.addHandler('ClanBattleService', 'deploySiegeArmy', (data, postData) => {
+    GvG.CountSieges(data.responseData.__class__);
+});
+
 FoEproxy.addHandler('ClanBattleService', 'getContinent', (data, postData) => {
-    GvG.ResetIndependences(data.responseData.continent.calculation_time.start_time);
+    GvG.Recalc(data.responseData.continent.calculation_time.start_time);
 	GvG.ShowGvgHud();
 });
 
@@ -13,6 +17,7 @@ FoEproxy.addHandler('AnnouncementsService', 'fetchAllAnnouncements', (data, post
 
 let GvG = {
     Independences: localStorage.getItem('GvGIndependencesCount') || 0,
+    Sieges: localStorage.getItem('GvGSiegesCount') || 0,
 	NextCalc: localStorage.getItem('GvGRecalcTime') || 0,
 
     /**
@@ -30,22 +35,21 @@ let GvG = {
 			});
 
 			$('body').append(div).promise().done(function() {
-				div.append('<p>'+GvG.Independences+'/4</p>')
-					.attr('title', 
-							'<h3>' + i18n('Global.BoxTitle') + '</h3>' + 
-							i18n('GvG.Independences.Tooltip') + '<br>' + 
-							'<em>' + i18n('GvG.Independences.Tooltip.Warning') + '</em')
-					.attr('data-placement','bottom')
+				div.append('<div class="independences">'+GvG.Independences+'/4</div><div class="sieges">'+GvG.Sieges+'</div>')
 					.tooltip(
 						{
+							'title': '<h3>' + i18n('Global.BoxTitle') + '</h3>' + i18n('GvG.Independences.Tooltip') + '<br>' + 
+								'<em>' + i18n('GvG.Independences.Tooltip.Warning') + '</em>',
 							'template': '<div class="tooltip foe-skin" role="tooltip"><div class="arrow"></div><div class="tooltip-inner"></div></div>',
+							'placement': 'bottom',
 							'html': true
 						}
 					);
 			});
 		}
 		else {
-			$('#gvg-hud p').text(GvG.Independences+'/4');
+			$('#gvg-hud .independences').text(GvG.Independences+'/4');
+			$('#gvg-hud .sieges').text(GvG.Sieges);
 		}
 	},
 
@@ -66,11 +70,8 @@ let GvG = {
 	 * @param data
 	 */
 	 CountIndepences: (data)=> {
-		let count = localStorage.getItem('GvGIndependencesCount');
+		let count = localStorage.getItem('GvGIndependencesCount') || 0;
 
-		if (count === null) {
-			count = 0;
-		}
 		if (data === "Success") {
 			count++;
 		}
@@ -81,22 +82,37 @@ let GvG = {
 	},
 
     /**
-	 * Reset Independence Counter after Recalc
+	 * Count Indepences on GvGMap
 	 * @param data
 	 */
-	 ResetIndependences: (calcTime)=> {
-		let storedRecalc = localStorage.getItem('GvGRecalcTime');
+	 CountSieges: (data)=> {
+		let count = localStorage.getItem('GvGSiegesCount') || 0;
 
-		if (storedRecalc === null) {
-			localStorage.setItem('GvGRecalcTime', calcTime);
+		if (data === "Success") {
+			count++;
 		}
-		else if (storedRecalc < calcTime) {
+
+		GvG.Sieges = count;
+		localStorage.setItem('GvGSiegesCount', count);
+		GvG.ShowGvgHud();
+	},
+
+    /**
+	 * Reset data after Recalc
+	 * @param calcTime
+	 */
+	 Recalc: (calcTime)=> {
+		let storedRecalc = localStorage.getItem('GvGRecalcTime') || 0;
+
+		if (storedRecalc != null && storedRecalc < calcTime) {
 			GvG.Independences = 0;
+			GvG.Sieges = 0;
 			localStorage.setItem('GvGIndependencesCount', GvG.Independences);
+			localStorage.setItem('GvGSiegesCount', GvG.Sieges);
+			localStorage.setItem('GvGRecalcTime', calcTime);
+			GvG.ShowGvgHud();
 		}
 
 		GvG.NextCalc = calcTime;
-		localStorage.setItem('GvGRecalcTime', calcTime);
-		GvG.ShowGvgHud();
 	},
 }
