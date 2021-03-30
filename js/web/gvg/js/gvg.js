f3fa85c8d495dd7e549229ee0fc5e262c0fdf720
outoftheline <yvi@outoftheline.net>
2021-03-30 09:14:15 +0200

    Tooltips

--- js/web/gvg/js/gvg.js
+++ js/web/gvg/js/gvg.js
@@ -48,9 +48,10 @@ let GvG = {
 
 			$('body').append(div).promise().done(function() {
 				div.append('<div class="independences">'+GvG.Independences+'/4</div><div class="sieges">'+GvG.Sieges+'</div>')
-					.attr('title', i18n('GvG.Independences.Tooltip') + '<em>' + i18n('GvG.Independences.Tooltip.Warning') + '</em>')
+					.attr('title', i18n('GvG.Independences.Tooltip') + '<br><em>' + i18n('GvG.Independences.Tooltip.Warning') + '</em>')
 					.tooltip(
 						{
+							useFoEHelperSkin: true,
 							headLine: i18n('Global.BoxTitle'),
 							placement: 'bottom',
 							html: true
