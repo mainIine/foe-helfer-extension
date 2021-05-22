8f3b37d56ee3eed912511a8343f20da6fa5334fd
Likeke181 <72246534+Likeke181@users.noreply.github.com>
2021-05-22 10:41:09 +0200

    singleMode im FP Collector disabled (#1997)
    * Allow date range in fp-collector
    
    Erlaubt eine Zeitspanne auszuwählen
    
    * Activate Negotiation Helper for GG
    
    * Revert "Activate Negotiation Helper for GG"
    
    This reverts commit 779ffcdfa98237eaba6686ba8b95284fb5246e6d.
    
    * FPCollector i18n improved
    
    (cherry picked from commit 8e7da5c90a310254420a0dfad86e146fb5023759)

--- js/web/fp-collector/js/fp-collector.js
+++ js/web/fp-collector/js/fp-collector.js
@@ -129,6 +129,7 @@ let FPCollector = {
 	minDateFilter: null,
 	maxDateFilter: moment(MainParser.getCurrentDate()).toDate(),
 	currentDateFilter: moment(MainParser.getCurrentDate()).format('YYYY-MM-DD'),
+	currentDateEndFilter: moment(MainParser.getCurrentDate()).format('YYYY-MM-DD'),
 
 	CityMapDataNew: null,
 
@@ -145,6 +146,7 @@ let FPCollector = {
 	 * @constructor
 	 */
 	ShowFPCollectorBox: async ()=> {
+		moment.locale(i18n('Local'));
 
 		if( $('#fp-collector').length < 1 )
 		{
@@ -208,7 +210,7 @@ let FPCollector = {
 			$('#fp-collectorBody').append(
 				`<div class="dark-bg head">
 					<div class="text-warning"><strong>${i18n('Boxes.FPCollector.TotalFP')} <span id="fp-collector-total-fp"></span></strong></div>
-					<div class="text-right"><button class="btn btn-default" id="FPCollectorPicker">${moment(FPCollector.currentDateFilter).format(i18n('Date'))}</button></div>
+					<div class="text-right"><button class="btn btn-default" id="FPCollectorPicker">${FPCollector.formatRange()}</button></div>
 				</div>`,
 				`<div id="fp-collectorBodyInner"></div>`
 			);
@@ -232,7 +234,7 @@ let FPCollector = {
 	buildBody: async ()=> {
 
 		let tr = [];
-		FPCollector.TodayEntries = await StrategyPoints.db['ForgePointsStats'].where('date').equals(FPCollector.currentDateFilter).toArray();
+		FPCollector.TodayEntries = await StrategyPoints.db['ForgePointsStats'].where('date').between(FPCollector.currentDateFilter, FPCollector.currentDateEndFilter, true, true).toArray();
 
 		$('#fp-collector-total-fp').text(await FPCollector.calculateTotal());
 
@@ -247,7 +249,7 @@ let FPCollector = {
 			for (const event of events)
 			{
 				const sumTotal = await FPCollector.calculateTotalByType(event);
-				const entriesEvent = await StrategyPoints.db['ForgePointsStats'].where({date: FPCollector.currentDateFilter, event: event}).toArray();
+				const entriesEvent = FPCollector.getEntriesByEvent(event);
 
 				tr.push(`<div class="foehelper-accordion ${event}">`);
 
@@ -331,10 +333,9 @@ let FPCollector = {
 	calculateTotal: async ()=> {
 		let totalFP = 0;
 
-		await StrategyPoints.db['ForgePointsStats']
-			.where('date')
-			.equals(FPCollector.currentDateFilter)
-			.each (entry => totalFP += entry.amount);
+		FPCollector.TodayEntries.forEach(e => {
+			totalFP += e.amount
+		});
 
 		return totalFP;
 	},
@@ -343,13 +344,12 @@ let FPCollector = {
 	calculateTotalByType: async (event)=> {
 		let totalFPByType = 0;
 
-		await StrategyPoints.db['ForgePointsStats']
-			.where({
-				date: FPCollector.currentDateFilter,
-				event: event
-			})
-			.each(entry => totalFPByType += entry.amount)
-		;
+		FPCollector.TodayEntries.forEach(e => {
+			if(e['event'] === event)
+			{
+				totalFPByType += e.amount
+			}
+		});
 
 		return totalFPByType;
 	},
@@ -370,7 +370,7 @@ let FPCollector = {
 			element: document.getElementById('FPCollectorPicker'),
 			format: i18n('Date'),
 			lang: MainParser.Language,
-			singleMode: true,
+			singleMode: false,
 			splitView: false,
 			numberOfMonths: 1,
 			numberOfColumns: 1,
@@ -378,14 +378,36 @@ let FPCollector = {
 			lockDays: FPCollector.lockDates,
 			minDate: FPCollector.minDateFilter,
 			maxDate: FPCollector.maxDateFilter,
+			startDate: FPCollector.currentDateFilter,
+			endDate: FPCollector.currentDateEndFilter,
 			showWeekNumbers: false,
-			onSelect: async (date)=> {
-				$('#FPCollectorPicker').text(`${moment(date).format(i18n('Date'))}`);
+			onSelect: async (dateStart, dateEnd)=> {
+				FPCollector.currentDateFilter = moment(dateStart).format('YYYY-MM-DD');
+				FPCollector.currentDateEndFilter = moment(dateEnd).format('YYYY-MM-DD');
 
-				FPCollector.currentDateFilter = moment(date).format('YYYY-MM-DD');
+				$('#FPCollectorPicker').text(FPCollector.formatRange());
 				await FPCollector.buildBody();
 			}
 		});
+	},
+
+
+	formatRange: ()=> {
+		let text = undefined;
+		let dateStart = moment(FPCollector.currentDateFilter);
+		let dateEnd = moment(FPCollector.currentDateEndFilter);
+
+		if (dateStart.isSame(dateEnd)){
+			text = `${dateStart.format(i18n('Date'))}`;
+		}
+		else if (dateStart.year() !== (dateEnd.year())){
+			text = `${dateStart.format(i18n('Date'))}` + ' - ' + `${dateEnd.format(i18n('Date'))}`;
+		}
+		else {
+			text = `${dateStart.format(i18n('DateShort'))}` + ' - ' + `${dateEnd.format(i18n('Date'))}`;
+		}
+
+		return text;
 	},
 
 
@@ -400,6 +422,20 @@ let FPCollector = {
 		});
 
 		return available;
+	},
+
+
+	getEntriesByEvent: (event)=> {
+		let entries = [];
+
+		FPCollector.TodayEntries.forEach(e => {
+			if(e['event'] === event)
+			{
+				entries.push(e)
+			}
+		});
+
+		return entries;
 	},
 
 
