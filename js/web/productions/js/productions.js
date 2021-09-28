1ccc79a86cf79a7951ed2c93eed86793dcf12d68
Gindi4711 <55417498+andrgin@users.noreply.github.com>
2021-09-28 23:16:31 +0200

    Productions: Fehler nach Besuchen des Außenpostens korrigiert

--- js/web/productions/js/productions.js
+++ js/web/productions/js/productions.js
@@ -641,7 +641,7 @@ let Productions = {
                 }
 				
 			}
-			else if(d['type'] === 'main_building') {
+			else if(d['id'] === 1) {
 				// Botschafter durchsteppen
 				if (MainParser.EmissaryService !== null) {
 
@@ -816,13 +816,13 @@ let Productions = {
 				sizetooltips = [];
       
 				// Gebäudegrößen für Effizienzberechnung laden
-				for (let i in MainParser.CityMapData) {
-					if (!MainParser.CityMapData.hasOwnProperty(i)) continue;
+				for (let i in Productions.CombinedCityMapData) {
+					if (!Productions.CombinedCityMapData.hasOwnProperty(i)) continue;
 
-					let BuildingSize = CityMap.GetBuildingSize(MainParser.CityMapData[i]);
+					let BuildingSize = CityMap.GetBuildingSize(Productions.CombinedCityMapData[i]);
 
-					sizes[MainParser.CityMapData[i]['cityentity_id']] = BuildingSize['total_area'];
-					sizetooltips[MainParser.CityMapData[i]['cityentity_id']] = (BuildingSize['street_area'] > 0 ? HTML.i18nReplacer(i18n('Boxes.Productions.SizeTT'), { 'streetnettosize': BuildingSize['street_area'] }) : '');
+					sizes[Productions.CombinedCityMapData[i]['cityentity_id']] = BuildingSize['total_area'];
+					sizetooltips[Productions.CombinedCityMapData[i]['cityentity_id']] = (BuildingSize['street_area'] > 0 ? HTML.i18nReplacer(i18n('Boxes.Productions.SizeTT'), { 'streetnettosize': BuildingSize['street_area'] }) : '');
 	            }
 
 			// einen Typ durchsteppen [money,supplies,strategy_points,...]
