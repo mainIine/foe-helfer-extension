a9a2b18d059553b683ff31a8bce98e050f094f0f
Daniel Siekiera <ds@optimondo.de>
2022-01-28 15:08:16 +0100

    kleiner Bugfix

--- js/web/stpatrickstats/js/stpatrickstats.js
+++ js/web/stpatrickstats/js/stpatrickstats.js
@@ -29,7 +29,7 @@ FoEproxy.addHandler('IdleGameService', 'getState', (data, postData) => {
 });
 
 FoEproxy.addHandler('IdleGameService', 'performActions', (data, postData) => {
-	
+
     if(postData[0]['requestClass'] !== 'IdleGameService')
     	return;
 
@@ -39,12 +39,16 @@ FoEproxy.addHandler('IdleGameService', 'performActions', (data, postData) => {
 	{
         let data2 = game[x];
 
+		if(!data2['characterId']) {
+			continue;
+		}
+
         if (data2.type === 'upgrade_level') {
-			stPatrick.stPat[data2.characterId].level += data2.amount;
+			stPatrick.stPat[data2['characterId']].level += data2.amount;
 		}
 
         if (data2.type === 'upgrade_manager') {
-			stPatrick.stPat[data2.characterId].manager += data2.amount;
+			stPatrick.stPat[data2['characterId']].manager += data2.amount;
 		}
     }
 
