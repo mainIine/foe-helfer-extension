71d5b45ede8c0714687076f2ecc9cb7048cd7868
Gindi4711 <55417498+andrgin@users.noreply.github.com>
2022-03-15 22:30:48 +0100

    loadBeta BugFix bei 2.8.0.0

--- js/web/_main/js/_main.js
+++ js/web/_main/js/_main.js
@@ -1259,7 +1259,7 @@ let MainParser = {
 		}
 		else if (LastStartedVersion !== extVersion) {
 			MainParser.StartUpType = 'UpdatedVersion';
-			if (!loadBeta) localStorage.removeItem('LoadBeta');
+			localStorage.removeItem('LoadBeta');
 			/* We have a new version installed and started the first time */
 		}
 		else if (LastAgreedVersion !== extVersion) {
