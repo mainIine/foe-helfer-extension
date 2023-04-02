20ba81da6c384e30c49dcc2d24f0ec6c903e1b83
Beelzebob6666 <6Beelzebob6@gmail.com>
2023-04-02 14:20:35 +0200

    stats: fx unit images

--- js/web/stats/js/stats.js
+++ js/web/stats/js/stats.js
@@ -827,6 +827,7 @@ let Stats = {
 				name: unitInfo.name,
 				era: era ? i18n('Eras.' + Technologies.Eras[era]) : '',
 				unitId,
+				unitUrl:srcLinks.get("/shared/unit_portraits/armyuniticons_50x50/armyuniticons_50x50_"+unitId+".jpg", true),
 				data: data.map(({date, army}) => [
 					+date,
 					army[unitId] || 0
@@ -837,7 +838,7 @@ let Stats = {
 			series,
 			pointFormat: `<tr>
 								<td>
-									<span class="unit_icon {series.options.unitId}" style="background-image:url('${srcLinks.get("/shared/unit_portraits/armyuniticons_50x50/armyuniticons_50x50_0.png", true)}')"></span>
+									<img src="{series.options.unitUrl}" style="width: 45px; height: 45px; border: 1px white solid; margin-right: 4px;"/>
 								</td>
 								<td>
 									<span style="margin: 0 5px;"><span style="color:{point.color}">●</span> {series.name}: </span>
@@ -1333,7 +1334,11 @@ let Stats = {
 		const seriesMapBySource = groupedByRewardSource[rewardSource] || {};
 		let serieData = Object.keys(seriesMapBySource).map(it => {
 			const rewardInfo = (rewardTypes.find(r => r.id === it) || {name: it});
-			const iconClass = rewardInfo.type === 'unit' ? `unit_icon ${rewardInfo.subType}" style="background-image:url('${srcLinks.get("/shared/unit_portraits/armyuniticons_50x50/armyuniticons_50x50_0.png", true)}')` : '';
+			const iconClass = "";
+			//if (rewardInfo.type === 'unit') {
+			//	iconClass = `unit_icon ${rewardInfo.subType}" style="background-image:url('${srcLinks.get("/shared/unit_portraits/armyuniticons_50x50/armyuniticons_50x50_"+rewardInfo.subType+".jpg", true)}')`;
+			//	console.log(rewardInfo)
+			//} 
 			// Asset image if not unit
 			let pointImage = '';
 			if (rewardInfo.type != 'unit') {
@@ -1356,13 +1361,25 @@ let Stats = {
 				if (url) {
 					pointImage = `<img src="${url}" style="width: 45px; height: 45px; margin-right: 4px;">`
 				}
+				return {
+					iconClass,
+					pointImage,
+					name: rewardInfo.name,
+					y: seriesMapBySource[it]
+				};
 			}
-			return {
-				iconClass,
-				pointImage,
-				name: rewardInfo.name,
-				y: seriesMapBySource[it]
-			};
+			else {
+				let url	= srcLinks.get("/shared/unit_portraits/armyuniticons_50x50/armyuniticons_50x50_"+rewardInfo.subType+".jpg", true);
+				pointImage = `<img src="${url}" style="width: 45px; height: 45px; margin-right: 4px;">`
+				console.log(rewardInfo)
+				return {
+					iconClass,
+					pointImage,
+					name: rewardInfo.name,
+					y: seriesMapBySource[it]
+				};
+			}
+
 		}).sort((a, b) => b.y - a.y);
 
 		if (Stats.state.filter !="") {
