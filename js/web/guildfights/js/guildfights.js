481ddcf9b832d416c06546a2a6deab04061c5112
outoftheline <yvi@outoftheline.net>
2023-11-05 10:41:39 +0100

    cardgame styling changes

--- js/web/guildfights/js/guildfights.js
+++ js/web/guildfights/js/guildfights.js
@@ -181,6 +181,7 @@ let GuildFights = {
 		let players = [];
 		let sumNegotiations = 0;
 		let sumBattles = 0;
+		let sumAttrition = 0;
 
 		for (let i in d)
 		{
@@ -191,6 +192,7 @@ let GuildFights = {
 			}
 			sumNegotiations += d[i]['negotiationsWon'] || 0;
 			sumBattles += d[i]['battlesWon'] || 0;
+			sumBattles += d[i]['attrition'] || 0;
 
 			players.push({
 				gbground: GuildFights.CurrentGBGRound,
@@ -199,7 +201,8 @@ let GuildFights = {
 				name: d[i]['player']['name'],
 				avatar: d[i]['player']['avatar'],
 				battlesWon: d[i]['battlesWon'] || 0,
-				negotiationsWon: d[i]['negotiationsWon'] || 0
+				negotiationsWon: d[i]['negotiationsWon'] || 0,
+				attrition: d[i]['attrition'] || 0
 			});
 		}
 
@@ -239,7 +242,8 @@ let GuildFights = {
 		{
 
 			let battles = 0,
-				negotiations = 0;
+				negotiations = 0,
+				attrition = 0;
 
 			let CurrentSnapshot = await GuildFights.db.snapshots
 				.where({
@@ -252,11 +256,13 @@ let GuildFights = {
 			{
 				battles = data.battles;
 				negotiations = data.negotiations;
+				attrition = data.attrition;
 			}
 			else
 			{
 				battles = data.diffbat;
 				negotiations = data.diffneg;
+				attrition = data.diffattr;
 			}
 
 			await GuildFights.db.snapshots.add({
@@ -266,7 +272,8 @@ let GuildFights = {
 				date: parseInt(moment.unix(data.time).format("YYYYMMDD")),
 				time: data.time,
 				battles: battles,
-				negotiations: negotiations
+				negotiations: negotiations,
+				attrition: attrition
 			});
 		}
 
@@ -532,8 +539,6 @@ let GuildFights = {
 				resize: true,
 				settings: 'GuildFights.ShowPlayerBoxSettings()'
 			});
-
-			// CSS in den DOM prügeln
 			HTML.AddCssFile('guildfights');
 		}
 
@@ -612,6 +617,7 @@ let GuildFights = {
 			player: 'player',
 			negotiationsWon: 'negotiations',
 			battlesWon: 'battles',
+			attrition: 'attrition',
 			total: 'total'
 		});
 
@@ -643,6 +649,7 @@ let GuildFights = {
 				negotaionAddOn = '',
 				diffNegotiations = 0,
 				diffBattles = 0,
+				diffAttr = 0,
 				newProgressClass = '',
 				change = false;
 
@@ -667,6 +674,13 @@ let GuildFights = {
 					{
 						diffBattles = playerNew['battlesWon'] - playerOld['battlesWon'];
 						fightAddOn = ' <small class="text-success">&#8593; ' + diffBattles + '</small>';
+						change = true;
+					}
+
+					if (playerOld['attrition'] < playerNew['attrition'])
+					{
+						diffAttr = playerNew['attrition'] - playerOld['attrition'];
+						fightAddOn = ' <small class="text-success">&#8593; ' + diffAttr + '</small>';
 						change = true;
 					}
 				}
@@ -701,6 +715,11 @@ let GuildFights = {
 			let both = playerNew['battlesWon'] + (playerNew['negotiationsWon'] * 2);
 			b.push(both);
 			b.push('</td>');
+
+			b.push('<td class="text-center">');
+			b.push(playerNew['attrition'] + fightAddOn);
+			b.push('</td>');
+
 			b.push('<td></td>');
 			b.push('</tr>');
 
@@ -709,6 +728,7 @@ let GuildFights = {
 				player: playerNew['name'],
 				negotiationsWon: playerNew['negotiationsWon'],
 				battlesWon: playerNew['battlesWon'],
+				battlesWon: playerNew['attrition'],
 				total: both
 			})
 		}
@@ -732,6 +752,8 @@ let GuildFights = {
 		t.push('<th class="text-center"><span class="negotiation" title="' + HTML.i18nTooltip(i18n('Boxes.GuildFights.Negotiations')) + '"></span> <strong class="text-warning">(' + HTML.Format(tN) + ')</strong></th>');
 		t.push('<th class="text-center"><span class="fight" title="' + HTML.i18nTooltip(i18n('Boxes.GuildFights.Fights')) + '"></span> <strong class="text-warning">(' + HTML.Format(tF) + ')</strong></th>');
 		t.push('<th class="text-center">' + i18n('Boxes.GuildFights.Total') + ' <strong class="text-warning">(' + HTML.Format(tNF) + ')</strong></th>');
+		t.push('<th class="text-center">' + i18n('Boxes.GuildFights.Attrition') + ' <strong class="text-warning">(' + HTML.Format(tNF) + ')</strong></th>');
+
 		t.push('<th></th>');
 		t.push('</tr>');
 		t.push('</thead>');
