b16a2d0745d73ee4ea2cb23f2080f5190400bd71
Beelzebob6666 <6Beelzebob6@gmail.com>
2023-12-20 22:12:48 +0100

    apply consistent text cleanup function across all modules

--- js/web/guildfights/js/guildfights.js
+++ js/web/guildfights/js/guildfights.js
@@ -940,7 +940,7 @@ let GuildFights = {
 				let sum = (e.battles + e.negotiations * 2);
 				h.push('<tr data-id="' + e.time + '" id="gbgtime_' + e.time + '">');
 				h.push(`<td class="is-number" data-number="${e.time}">${moment.unix(e.time).format(i18n('DateTime'))}</td>`);
-				h.push(`<td class="case-sensitive" data-text="${e.name.toLowerCase().replace(/[\W_ ]+/g, "")}">${e.name}</td>`);
+				h.push(`<td class="case-sensitive" data-text="${helper.str.cleanup(e.name)}">${e.name}</td>`);
 				h.push(`<td class="is-number text-center" data-number="${e.negotiations}">${HTML.Format(e.negotiations)}</td>`);
 				h.push(`<td class="is-number text-center" data-number="${e.battles}">${HTML.Format(e.battles)}</td>`);
 				h.push(`<td class="is-number text-center" data-number="${sum}">${HTML.Format(sum)}</td>`);
