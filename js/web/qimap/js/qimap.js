abfb4911f726b406abe205fe37849d97fc8e2a9e
outoftheline <yvi@outoftheline.net>
2024-03-26 00:10:51 +0100

    Polynesia goods, QI Map tinkering

--- js/web/qimap/js/qimap.js
+++ js/web/qimap/js/qimap.js
@@ -1,5 +1,14 @@
 const QIMap = {
     CurrentMapData: {},
+    NodeConnections: [],
+    MaxX: 0,
+    MaxY: 0,
+    MinX: 100,
+    MinY: 100,
+    XMultiplier: 60,
+    YMultiplier: 50,
+    XOffset: 60,
+    YOffset: 10,
 
     init: (responseData) => {
         QIMap.CurrentMapData = responseData
@@ -29,27 +38,51 @@ const QIMap = {
 
     showBody: () => {
         let out = '<div id="nodeMap">'
-        let maxX = 0 
-        let maxY = 0
-        let minX = 100
-        let minY = 100
+        
+		QIMap.Map = document.createElement("canvas");
+		QIMap.MapCTX = QIMap.Map.getContext('2d');
+
+		$(QIMap.Map).attr({
+			id: 'nodeConnections',
+			width: ProvinceMap.Size.width,
+			height: ProvinceMap.Size.height,
+		});
+
         QIMap.CurrentMapData.nodes.forEach(node => {
             let x = (node.position.x ? node.position.x : 0)
-            maxX = (x > maxX ? x : maxX)
-            maxY = ((node.position.y || 0) > maxY ? (node.position.y || 0) : maxY)
-            minX = (x < minX ? x : minX)
-            minY = ((node.position.y || 0) < minY ? (node.position.y || 0) : minY)
+            QIMap.MaxX = (x > QIMap.MaxX ? x : QIMap.MaxX)
+            QIMap.MaxY = ((node.position.y || 0) > QIMap.MaxY ? (node.position.y || 0) : QIMap.MaxY)
+            QIMap.MinX = (x < QIMap.MinX ? x : QIMap.MinX)
+            QIMap.MinY = ((node.position.y || 0) < QIMap.MinY ? (node.position.y || 0) : QIMap.MinY)
+
+            // BUG works the first time, then adds more to the array
+            node.connectedNodes.forEach(connection => {
+                let findDuplicates = QIMap.NodeConnections.find(x => x.id === connection.targetNodeId)
+                if (!findDuplicates && findDuplicates?.connectedNode !== node.id) {
+                    let newNode = {
+                        id: node.id, 
+                        nodePosition: node.position,
+                        connectedNode: connection.targetNodeId, 
+                        connectedNodePosition: connection.pathTiles[0]
+                    }
+                    if (findDuplicates !== newNode)
+                        QIMap.NodeConnections.push(newNode)
+                }
+            })
         })
+
+        //console.log(QIMap.NodeConnections)
+        QIMap.showNodeConnections()
+
         QIMap.CurrentMapData.nodes.forEach(node => {
-            console.log(node)
-            let x = (node.position.x - minX) * 4 + 1 || 1 
-            let y = (node.position.y - minY) * 3 + 1 || 1
+            let x = (node.position.x - QIMap.MinX) * QIMap.XMultiplier + QIMap.XOffset || QIMap.XOffset
+            let y = (node.position.y - QIMap.MinY) * QIMap.YMultiplier + QIMap.YOffset || QIMap.YOffset
             let type = (node.type.type !== undefined ? node.type.type : node.type.fightType)
             let currentProgress = (node.state.state === "open" ? (node.state.currentProgress || 0) + "/" : (node.state.state === "finished") ? node.type.requiredProgress + "/" : '')
             if (node.type.__class__ !== "GuildRaidsMapNodeStart") {
-                out += '<span style="left:'+x+'em;top:'+y+'em" class="'+node.state.state+ " " + type + " " + (node.type.armyType ? node.type.armyType : '') + (node.state.hasTarget ? ' target' : '') + '">'
+                out += '<span id="'+ node.id +'" style="left:'+x+'px;top:'+y+'px" class="'+node.state.state+ " " + type + " " + (node.type.armyType ? node.type.armyType : '') + (node.state.hasTarget ? ' target' : '') + '">'
                     out += '<span class="img"></span>'
-                    out += '<b></b>'+currentProgress + node.type.requiredProgress
+                    out += '<b></b>'+node.id +" "+currentProgress + node.type.requiredProgress
                     if (node.mapEffects?.effectActiveBeforeFinish?.boosts) {
                         out += '<br>'
                         node.mapEffects.effectActiveBeforeFinish.boosts.forEach(boost => {
@@ -65,12 +98,31 @@ const QIMap = {
                 out +='</span>'
             }
             else 
-                out += '<span style="left:'+x+'em;top:'+y+'em" class="start"><span class="img"></span></span>'
+                out += '<span style="left:'+x+'px;top:'+y+'px" class="start"><span class="img"></span></span>'
         })
         out += '</div>'
         
         $('#QIMap').find('#QIMapBody').html(out).promise().done(function () {
-            $('#QIMapBody').css({'height': maxY*3+9+'em','width': maxX*4+9+'em'})
+            $('#nodeMap').append(QIMap.Map)
+            $('#QIMapBody, #nodeConnections').css({'width': QIMap.MaxY*QIMap.XMultiplier+QIMap.XOffset+'px','height': QIMap.MaxY*QIMap.YMultiplier+QIMap.YOffset+'px'})
+        })
+    },
+
+    showNodeConnections: () => {
+        QIMap.NodeConnections.forEach(connection => {
+            let addLength = ((connection.nodePosition.x || 0) === (connection.connectedNodePosition.x || 0) ? 60 : 0)
+            let x = ((connection.nodePosition.x || 0) - QIMap.MinX) * QIMap.XMultiplier + QIMap.XOffset || QIMap.XOffset
+            let y = (connection.nodePosition.y - QIMap.MinY) * QIMap.YMultiplier + QIMap.YOffset || QIMap.YOffset
+            let targetX = ((connection.connectedNodePosition.x || 0) - QIMap.MinX) * QIMap.XMultiplier + QIMap.XOffset || QIMap.XOffset
+            let targetY = (connection.connectedNodePosition.y - QIMap.MinY) * QIMap.YMultiplier + QIMap.YOffset || QIMap.YOffset
+    
+            QIMap.MapCTX.beginPath()
+            QIMap.MapCTX.moveTo(x, y)
+            QIMap.MapCTX.lineTo(targetX, targetY)
+            QIMap.MapCTX.closePath()
+            QIMap.MapCTX.stroke()
+
+            //console.log(x,y,targetX,targetY)
         })
     }
 }
