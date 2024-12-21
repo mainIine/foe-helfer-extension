d246b33e9236b50bccda6d8b8f3ea819aafe5c2b
Beelzebob6666 <6Beelzebob6@gmail.com>
2024-12-21 11:42:00 +0100

    new moueActions handler + repeat last building

--- js/web/reconstruction/js/reconstruction.js
+++ js/web/reconstruction/js/reconstruction.js
@@ -66,7 +66,7 @@ FoEproxy.addRequestHandler('CityReconstructionService', 'saveDraft', (data) => {
                 reconstruction.pageUpdate(id)
                 pagesUpdated=true
             }
-            FoEproxy.triggerFoeHelperHandler('ReconstructionBuildingPlaced',x.entityId)
+            FoEproxy.triggerFoeHelperHandler('ReconstructionBuildingPlaced',{id:x.entityId,last:(reconstruction.count[id].stored==0)})
         } else if (!x.position) {
             reconstruction.count[id].placed--
             reconstruction.count[id].stored++    
