282789b4554fb3afce5f28e94f65f32fc01d0707
outoftheline <yvi@outoftheline.net>
2025-02-15 22:35:03 +0100

    Profile: add settlements and city QI boosts

--- js/web/profile/js/profile.js
+++ js/web/profile/js/profile.js
@@ -16,10 +16,12 @@ const Profile = {
     goods: {},
     guildGoods: 0,
     battleBoosts: {},
+    settlements: [],
 
     init: (responseData) => {
         Profile.daysPlayed = responseData.achievementGroups.find(x => x.id == "special").achievements.find(x => x.id == 'foe_fanclub').currentLevel.progress || null
-        Profile.showButton()
+        Profile.showButton();
+        Profile.settlements = responseData.achievementGroups.find(x => x.id == "cultural_settlements").achievements;
     },
 
 	showButton: () => {
@@ -90,11 +92,11 @@ const Profile = {
         content.push('<span class="fp">' + HTML.Format(parseInt(Profile.fpProduction)) + ", " + i18n('General.Boost')+ ' ' +Boosts.Sums.forge_points_production + '%</span><br>');
         content.push('<div class="goods">')
         if (Profile.goods[CurrentEraID-2])
-            content.push('<span class="prev">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID-2])) || 0) + '</span>');
+            content.push('<span class="prev">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID-2])) || 0) + '</span> ');
         if (Profile.goods[CurrentEraID-1])
-            content.push('<span class="current">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID-1])) || 0) + '</span>');
+            content.push('<span class="current">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID-1])) || 0) + '</span> ');
         if (Profile.goods[CurrentEraID])
-            content.push('<span class="next">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID])) || 0) + '</span>');
+            content.push('<span class="next">' + HTML.Format(parseInt(parseInt(Profile.goods[CurrentEraID])) || 0) + '</span> ');
         if (Profile.guildGoods)
             content.push('<span class="guild">' + HTML.Format(parseInt(parseInt(Profile.guildGoods)) || 0) + '</span>')
         content.push('</div>');
@@ -120,11 +122,47 @@ const Profile = {
         content.push('<tr><td><span class="aAtt">'+HTML.Format(parseInt(Boosts.Sums['guild_raids-att_boost_attacker_no_settlement']))+'</span><span class="aDef">'+HTML.Format(parseInt(Boosts.Sums['guild_raids-def_boost_attacker_no_settlement']))+'</span></td><td><span class="qi"></span></td><td><span class="dAtt">'+HTML.Format(parseInt(Boosts.Sums['guild_raids-att_boost_defender_no_settlement']))+'</span><span class="dDef">'+HTML.Format(parseInt(Boosts.Sums['guild_raids-def_boost_defender_no_settlement']))+'</span></td></tr>');
         content.push('</tr></table>');
         content.push('</div>');
-        /** for later use
-        content.push('<div class="qiBoosts pad">');
-        content.push('<span class="qiactions">' + HTML.Format(parseInt(Boosts.Sums.guild_raids_action_points_collection_no_settlement)) + '</span><br>');
-        content.push('</div>');
-        */
-        $('#PlayerProfileBody').html(content.join(''));
+
+        if (Profile.settlements.length > 0) {
+            content.push('<div class="settlements pad text-center">');
+            content.push('<h2>'+i18n('Boxes.PlayerProfile.Settlements')+'</h2>');
+            for (let settlement of Profile.settlements) {
+                content.push('<span class="'+settlement.id+'" data-original-title="'+settlement.name+'">');
+                content.push('<img src="'+srcLinks.get(`/shared/icons/achievements/achievement_icons_${settlement.id}.png`,true)+'" />')
+                content.push(HTML.Format(parseInt(settlement.currentLevel.progress)) + '</span>');
+            }
+            content.push('</div>');
+        }
+
+        let hasQIBoosts = (Boosts.Sums.guild_raids_action_points_collection_no_settlement+Boosts.Sums.guild_raids_coins_production+Boosts.Sums.guild_raids_coins_start+Boosts.Sums.guild_raids_supplies_production+Boosts.Sums.guild_raids_supplies_start+Boosts.Sums.guild_raids_goods_start+Boosts.Sums.guild_raids_units_start !== 0)
+        if (hasQIBoosts) {
+            content.push('<div class="qiBoosts pad text-center">');
+            content.push('<h2>'+i18n('Boxes.PlayerProfile.QIBoosts')+'</h2>');
+            if (Boosts.Sums.guild_raids_action_points_collection_no_settlement !== 0)
+                content.push('<span class="qiactions">' + HTML.Format(parseInt(Boosts.Sums.guild_raids_action_points_collection_no_settlement)) + '</span> ');
+            if (Boosts.Sums.guild_raids_coins_production !== 0)
+                content.push('<span class="qicoins">' + HTML.Format(parseInt(Boosts.Sums.guild_raids_coins_production)) + '%</span> ');
+            if (Boosts.Sums.guild_raids_coins_start !== 0)
+                content.push('<span class="qicoins_start">+' + HTML.Format(parseInt(Boosts.Sums.guild_raids_coins_start)) + '</span> ');
+            if (Boosts.Sums.guild_raids_supplies_production !== 0)
+                content.push('<span class="qisupplies">' + HTML.Format(parseInt(Boosts.Sums.guild_raids_supplies_production)) + '%</span> ');
+            if (Boosts.Sums.guild_raids_supplies_start !== 0)
+                content.push('<span class="qisupplies_start">+' + HTML.Format(parseInt(Boosts.Sums.guild_raids_supplies_start)) + '</span> ');
+            if (Boosts.Sums.guild_raids_goods_start !== 0)
+                content.push('<span class="qigoods_start">+' + HTML.Format(parseInt(Boosts.Sums.guild_raids_goods_start)) + '</span> ');
+            if (Boosts.Sums.guild_raids_units_start !== 0)
+                content.push('<span class="qiunits_start">+' + HTML.Format(parseInt(Boosts.Sums.guild_raids_units_start)) + '</span> ');
+            content.push('</div>');
+        }
+
+        content.push('<span class="toggleMore">&nbsp;</span>');
+
+        $('#PlayerProfileBody').html(content.join('')).promise().done(function(){
+			$('#PlayerProfileBody').on('click', '.toggleMore', function () {
+				$(this).toggleClass('active');
+				$('#PlayerProfileBody .qiBoosts, #PlayerProfileBody .settlements').slideToggle();
+			});
+            $('#PlayerProfileBody [data-original-title]').tooltip();
+        });
     }
 }
