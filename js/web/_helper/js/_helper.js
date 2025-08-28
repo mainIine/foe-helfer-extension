9f666b294a8dd0308f8866d1809c5f2e39fd9a34
Beelzebob6666 <6Beelzebob6@gmail.com>
2025-08-28 21:15:37 +0200

    Format - fix bug

--- js/web/_helper/js/_helper.js
+++ js/web/_helper/js/_helper.js
@@ -676,7 +676,7 @@ let HTML = {
 		if (number === 0) {
 			return '-';
 		} else {
-			if (typeof number !== 'number' || isNaN(Number(number))) return "" + number;
+			if (typeof number !== 'number' && isNaN(Number(number))) return "" + number;
 			return Number(number).toLocaleString(i18n('Local'));
 		}
 	},
