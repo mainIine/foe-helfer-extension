943356d4056ab74a65bc7034069520813250329a
Beelzebob6666 <6beelzebob6@gmail.com>
2025-11-04 10:51:31 +0100

    negotiation - add autoclose exemption

--- js/web/negotiation/js/negotiation.js
+++ js/web/negotiation/js/negotiation.js
@@ -997,7 +997,21 @@ FoEproxy.addHandler('all','all', (data, postData) => {
 		return
 	}
 	if (!(
-		["RankingService","QuestService","ResourceService","ResourceShopService","TimeService", "MessageService", "WorldChallengeService", "AutoAidService", "TrackingService", "AnnouncementService","InventoryService","GuildExpeditionNotificationService"].includes(data.requestClass) ||
+		[
+			"RankingService",
+			"QuestService",
+			"ResourceService",
+			"ResourceShopService",
+			"TimeService", 
+			"MessageService", 
+			"WorldChallengeService", 
+			"AutoAidService", 
+			"TrackingService", 
+			"AnnouncementService",
+			"InventoryService",
+			"GuildExpeditionNotificationService",
+			"FriendsTavernService"
+		].includes(data.requestClass) ||
 		data.requestMethod === 'markContributionNotificationsRead'
 	)) {
 		Negotiation.ExitNegotiation()
