3c6863ac786dfd1e328792f62f94d3fab4f03a13
Beelzebob6666 <6Beelzebob6@gmail.com>
2025-12-02 19:21:17 +0100

    allies - fix boosts

--- js/web/_main/js/_main.js
+++ js/web/_main/js/_main.js
@@ -1655,7 +1655,7 @@ let MainParser = {
 					rooms[0+"#" + unassigned] = {
 						allyRarity: x.rarity?.value || "",
 						allyLevel: x.level || null,												
-						allyBoosts: x.boosts || null,
+						allyBoosts: x.currentLevel?.boosts || x.boosts || null,
 						allyName: MainParser.Allies.meta[x.allyId]?.name || "",
 					}
 					unassigned++
@@ -1669,7 +1669,7 @@ let MainParser = {
 						roomRarity: r.rarity?.value || Object.keys(MainParser.Allies.rarities).join("#"),
 						allyRarity: r.ally?.rarity?.value || "",
 						allyLevel: r.ally?.level || null,												
-						allyBoosts: r.ally?.boosts || null,
+						allyBoosts: r.ally?.currentLevel?.boosts || r.ally?.boosts || null,
 						allyName: MainParser.Allies.meta[r.ally?.allyId]?.name || "",
 					}
 				}
