1c3bb594e20a966bc9224429fe84dc0dc3eb7aa4
Beelzebob6666 <6Beelzebob6@gmail.com>
2026-06-23 18:45:15 +0200

    add icons for castle points and quantum medals - stats: add castle points

--- js/web/stats/js/stats.js
+++ js/web/stats/js/stats.js
@@ -260,7 +260,7 @@ let Stats = {
 	isVisitingCulturalOutpost: false,
 	goodsSubTypes:[],
 	ResMap: {
-		NoAge: ['money', 'supplies', 'tavern_silver', 'medals', 'premium', 'guild_raids_medals'],
+		NoAge: ['money', 'supplies', 'tavern_silver', 'medals', 'premium', 'guild_raids_medals', 'castle_points'],
 		special: ['promethium', 'orichalcum', 'mars_ore', 'asteroid_ice', 'venus_carbon', 'unknown_dna','crystallized_hydrocarbons','dark_matter'],
 	},
 
